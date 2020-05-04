f5faa2953e20f7d311d1f5167a96089561ed64e7
Leandro <syiabonga111@gmail.com>
2020-05-04 05:29:49 -0500

    addtodo component

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Add Todo</h3>
+    <div class="add">
+      <form @submit="onSubmit">
+        <div class="form-group">
+            <input class="form-control col-md-4" type="text" v-model="title" placeholder="Add Todo...">
+        </div>
+        <input class="btn btn-dark" type="submit" value="Submit">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AddTodo'
+}
+</script>
+
+<style>
+
+</style>
