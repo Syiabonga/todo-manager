2a59b7e87b2f39725fce563bc0e4ad56f4686bfa
Leandro <syiabonga111@gmail.com>
2020-05-04 06:27:40 -0500

    filter todos by number of elements

---
+++ src/components/FilterTodo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h3>Filter Todos</h3>
+      <select @change="filterTodos($event)">
+        <option value="200">200</option>
+        <option value="200">100</option>
+        <option value="200">50</option>
+        <option value="200">20</option>
+        <option value="200">10</option>
+        <option value="200">5</option>
+      </select>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "FilterTodos",
+  methods: mapActions(["filterTodos"])
+};
+</script>
+
+<style>
+
+</style>
